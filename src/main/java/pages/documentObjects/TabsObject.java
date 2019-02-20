3102ff0c611ed16ac8f4688a592b006b300da7bd
dmitriy.martynenko <dm.martyn1311@gmail.com>
2019-02-20 08:57:09 +0200

    refactor tabsObject

--- src/main/java/pages/documentObjects/TabsObject.java
+++ src/main/java/pages/documentObjects/TabsObject.java
@@ -25,60 +25,60 @@ public class TabsObject {
     }
 
     private ArrayList<TabsData> TabsObject1() {
-        ArrayList<TabsData> tabsArrayObjects1 = new ArrayList<>();
+        ArrayList<TabsData> tabsArrayObjects = new ArrayList<>();
 
-        tabsArrayObjects1.add(TabsData.builder().name("").formCode("J0100114").build());
-        tabsArrayObjects1.add(TabsData.builder().name("Тест1").formCode("S0501408").build());
-        tabsArrayObjects1.add(TabsData.builder().name("Тест під номером 1").formCode("S0100113").build());
-        tabsArrayObjects1.add(TabsData.builder().name("Test 1").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("").formCode("J0100114").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест1").formCode("S0501408").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест під номером 1").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 1").formCode("S0100113").build());
 
-        return tabsArrayObjects1;
+        return tabsArrayObjects;
     }
 
     private ArrayList<TabsData> TabsObject2() {
-        ArrayList<TabsData> tabsArrayObjects2 = new ArrayList<>();
+        ArrayList<TabsData> tabsArrayObjects = new ArrayList<>();
 
-        tabsArrayObjects2.add(TabsData.builder().name("").formCode("J0100114").build());
-        tabsArrayObjects2.add(TabsData.builder().name("Тест2").formCode("S0501408").build());
-        tabsArrayObjects2.add(TabsData.builder().name("Тест під номером 2").formCode("S0100113").build());
-        tabsArrayObjects2.add(TabsData.builder().name("Test 2").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("").formCode("J0100114").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест2").formCode("S0501408").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест під номером 2").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 2").formCode("S0100113").build());
 
-        return tabsArrayObjects2;
+        return tabsArrayObjects;
     }
 
     private ArrayList<TabsData> TabsObject3() {
-        ArrayList<TabsData> tabsArrayObjects3 = new ArrayList<>();
+        ArrayList<TabsData> tabsArrayObjects = new ArrayList<>();
 
-        tabsArrayObjects3.add(TabsData.builder().name("").formCode("J0100114").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест3").formCode("S0501408").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест під номером 4").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 3").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 3-1").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 3-2").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 3-3").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("").formCode("J0100114").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест3").formCode("S0501408").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест під номером 4").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 3").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 3-1").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 3-2").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 3-3").formCode("S0100113").build());
 
-        return tabsArrayObjects3;
+        return tabsArrayObjects;
     }
 
     private ArrayList<TabsData> TabsObject4() {
-        ArrayList<TabsData> tabsArrayObjects3 = new ArrayList<>();
+        ArrayList<TabsData> tabsArrayObjects = new ArrayList<>();
 
-        tabsArrayObjects3.add(TabsData.builder().name("").formCode("J0100114").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест4").formCode("S0501408").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест під номером 4").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 4").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("").formCode("J0100114").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест4").formCode("S0501408").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест під номером 4").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 4").formCode("S0100113").build());
 
-        return tabsArrayObjects3;
+        return tabsArrayObjects;
     }
 
     private ArrayList<TabsData> TabsObject5() {
-        ArrayList<TabsData> tabsArrayObjects3 = new ArrayList<>();
+        ArrayList<TabsData> tabsArrayObjects = new ArrayList<>();
 
-        tabsArrayObjects3.add(TabsData.builder().name("").formCode("J0100114").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест5").formCode("S0501408").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Тест під номером 5").formCode("S0100113").build());
-        tabsArrayObjects3.add(TabsData.builder().name("Test 5").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("").formCode("J0100114").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест5").formCode("S0501408").build());
+        tabsArrayObjects.add(TabsData.builder().name("Тест під номером 5").formCode("S0100113").build());
+        tabsArrayObjects.add(TabsData.builder().name("Test 5").formCode("S0100113").build());
 
-        return tabsArrayObjects3;
+        return tabsArrayObjects;
     }
 }
