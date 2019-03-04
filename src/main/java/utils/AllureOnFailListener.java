3b32ecb2e5a91f95fba902f9a131b663edc591a9
dmitriy.martynenko <dm.martyn1311@gmail.com>
2019-03-04 15:41:35 +0200

    refactor screenshot

--- src/main/java/utils/AllureOnFailListener.java
+++ src/main/java/utils/AllureOnFailListener.java
@@ -1,53 +1,77 @@
 package utils;
 
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import io.qameta.allure.Attachment;
 import lombok.extern.log4j.Log4j;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
 import java.io.IOException;
 
 @Log4j
 public final class AllureOnFailListener implements ITestListener {
 
     @Override
-    public void onTestStart(ITestResult iTestResult) {
-        log.info("Start test: " + iTestResult.getName());
-
+    public void onTestStart(final ITestResult result) {
+        log.info("Start checkAndWaitPeriod:" + result.getName());
     }
 
     @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-        log.info("Test: " + iTestResult.getName() + " successfully ended");
+    public void onTestSuccess(final ITestResult result) {
+        log.info("Test" + result.getName() + " successfully ended");
     }
 
     @Override
-    public void onTestFailure(ITestResult iTestResult) {
+    public void onTestFailure(final ITestResult result) {
         try {
-            new DriverUtil().screenshot();
+            screenshot();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        log.info("Test " + iTestResult.getName() + " ended failed!");
+        log.info("Test" + result.getName() + "ended failed");
     }
 
     @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-
+    public void onTestSkipped(final ITestResult result) {
+        try {
+            screenshot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
+    public void onTestFailedButWithinSuccessPercentage(final ITestResult result) {
+        try {
+            screenshot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onStart(ITestContext iTestContext) {
-
+    public void onStart(final ITestContext context) {
     }
 
     @Override
-    public void onFinish(ITestContext iTestContext) {
+    public void onFinish(final ITestContext context) {
 
     }
+
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+
+        if (screenshot == null) {
+            screenshot = new File(new File("src/main/resources/supportFiles/screen.png").getAbsolutePath());
+
+        }
+
+        return Files.toByteArray(screenshot);
+    }
+
 }
