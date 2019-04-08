e23250bdf0ee60a1aca572998b32af2fa3914b5e
dimonklas <dm.martyn1311@gmail.com>
2019-04-08 10:43:02 +0300

    изменил ArrayList на List

--- src/main/java/pages/document/s1605110/FormDataS1605110.java
+++ src/main/java/pages/document/s1605110/FormDataS1605110.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 import pages.document.FormDataAbstract;
 import pages.document.FormFields;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Getter
 public class FormDataS1605110 extends FormDataAbstract {
     private final String CODE = "S1605110";
     private final String DESCRIPTION = "51-авто. Звіт про перевезення вантажів та пасажирів автомобільним транспортом";
-    private ArrayList<FormFields> fields;
+    private List<FormFields> fields;
 }
